dbae3f62228cd03a50da15ae26a9c65eef50046e
DrkD3vil <bijoydeyemon3@gmail.com>
2024-11-29 13:33:14 +0600

    toaster message added

--- resources/views/admin/admin_layout.blade.php
+++ resources/views/admin/admin_layout.blade.php
@@ -27,6 +27,9 @@
     <!-- App Css-->
     <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
+    <!-- Include Toastr CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
 </head>
 
 <body data-topbar="dark">
@@ -95,6 +98,33 @@
 
     <!-- App js -->
     <script src="{{ asset ('backend/assets/js/app.js')}}"></script>
+
+    <!-- Include Toastr JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type','info') }}"
+        switch (type) {
+            case 'info':
+                toastr.info(" {{ Session::get('message') }} ");
+                break;
+
+            case 'success':
+                toastr.success(" {{ Session::get('message') }} ");
+                break;
+
+            case 'warning':
+                toastr.warning(" {{ Session::get('message') }} ");
+                break;
+
+            case 'error':
+                toastr.error(" {{ Session::get('message') }} ");
+                break;
+        }
+        @endif
+    </script>
+
+
 </body>
 
 </html>
